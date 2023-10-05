c6beb9f1cd53895d25a28ce0221ac6156c9924f5
Leal Emmanuel <emmanuel_leal@outlook.com.ar>
2023-10-05 18:55:20 -0300

    encuestas v2 con codium

--- src/componets/NotFound.jsx
+++ src/componets/NotFound.jsx
@@ -2,6 +2,7 @@ import React from "react";
 
 const NotFound = () => {
   return (
+    
     <div>
       <h2>Página no encontrada</h2>
       <p>Lo sentimos, la página que estás buscando no existe.</p>
